f447d1c391b8b6c50933d5f261c542c0a5f4104e
joshuaposton0105 <joshl.poston@gmail.com>
2025-09-06 15:00:56 -0500

    Fix page scroll position on route changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
@@ -11,6 +12,7 @@ import Resources from './pages/Resources';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-white">
         <Header />
         <main>
